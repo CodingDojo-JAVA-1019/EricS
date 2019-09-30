86e45cf6d51a6eb0a9c704006718ae9daa5f737a
smogdog24 <49054191+smogdog24@users.noreply.github.com>
2019-09-30 13:45:42 -0500

    First Java Program - day 1

---
+++ Day1/First_java/FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Eric");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Chicago, IL");
+
+    }
+}
