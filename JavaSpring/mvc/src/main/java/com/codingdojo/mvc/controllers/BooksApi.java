cfb8497537f3f4296f6e441c76177043b1b6926d
smogdog24 <49054191+smogdog24@users.noreply.github.com>
2019-10-17 11:41:34 -0500

    MVC-books
    mvc-books spring data 1 - CRUD

--- JavaSpring/mvc/src/main/java/com/codingdojo/mvc/controllers/BooksApi.java
+++ JavaSpring/mvc/src/main/java/com/codingdojo/mvc/controllers/BooksApi.java
@@ -21,31 +21,36 @@ public class BooksApi {
 		this.bookService = bookService;
 	}
 	
+//	@RequestMapping("/")
+//		public index() {
+//		return index.jsp;
+//	}
+	
 	 @RequestMapping("/api/books")
 	    public List<Book> index() {
 	        return bookService.allBooks();
 	}
-	    @RequestMapping(value="/api/books", method=RequestMethod.POST)
-	    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
-	        Book book = new Book(title, desc, lang, numOfPages);
-	        return bookService.createBook(book);
-	    }
-	    
-	    @RequestMapping("/api/books/{id}")
-	    public Book show(@PathVariable("id") Long id) {
-	        Book book = bookService.findBook(id);
-	        return book;
-	    }
-	    
-	    @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
-	    public Book update(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
-	        Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
-	        return book;
-	    }
-	    
-	    @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
-	    public void destroy(@PathVariable("id") Long id) {
-	        bookService.deleteBook(id);
-	    }
-	    
+    @RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        return bookService.createBook(book);
+    }
+    
+    @RequestMapping("/api/books/{id}")
+    public Book show(@PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        return book;
+    }
+    
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+    public Book update(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
+        return book;
+    }
+    
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+    public void destroy(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+    }
+    
 }
